bf440baed02a0f4e55c2c48eea43c8e45cc2e856
Yunus Emre Erkaçmaz <92800866+yunusemreerkacmaz@users.noreply.github.com>
2025-05-05 13:32:14 +0300

    migration commit

---
+++ api/DataAccess/Migrations/20250505082232__mig34.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _mig34 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApprovingAuthorityId",
+                table: "Barcodes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovingAuthorityId",
+                table: "Barcodes");
+        }
+    }
+}
